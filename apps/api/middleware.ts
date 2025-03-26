bf3c0c7a8e031f990c9053195e55732d59b2eab1
Yash-KK <reachyashk@gmail.com>
2025-03-26 10:25:25 +0530

    seed the db and static-middleware

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,23 +1,19 @@
-import { type NextFunction, type Request, type Response } from "express";
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_PUBLIC_KEY } from "./config";
 
-export const authMiddleware = (
+export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-  const clerkToken = req.headers.authorization
-  req.userId = clerkToken!; // hardcoded for now, will be fetched from clerk
+) {
+  // const token = req.headers["authorization"];
+  // if (!token) {
+  // res.status(401).json({ error: "Unauthorized" });
+  // return;
+  // }
+
+  // req.userId = token.split(".")[0] as string;
+  req.userId = "2";
   next();
-};
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
+}
